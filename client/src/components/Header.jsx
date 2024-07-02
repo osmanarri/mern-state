f95a0fbdb36235d1afc8ea289204289f10f2477d
osmanarri <osmanarri1984@gmail.com>
2024-07-02 10:31:34 -0400

    comple register page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
             <ul className='flex gap-4'>
                 <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li></Link>
                 <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li></Link>
-                <Link to='/login'><li className='text-slate-700 hover:underline cursor-pointer'>Login in</li></Link>               
+                <Link to='/login'><li className='text-slate-700 hover:underline cursor-pointer'>Login</li></Link>               
             </ul>
         </div>    
     </header>
