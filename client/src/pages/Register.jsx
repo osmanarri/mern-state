2981fb33901874710a365f8d47ed931cfa3d881c
osmanarri <osmanarri1984@gmail.com>
2024-07-02 11:15:17 -0400

    complete register page functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,36 +1,103 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Register() {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      // Spread operator to keep the old info
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  // Submit the form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError(null); // Reset error state
+    try {
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      
+      // success
+      const data = await res.json();
+      navigate('/login')
+
+      if (!res.ok) {
+        setError(data.message || 'An error occurred');
+        setIsLoading(false);
+        return;
+      }
+
+      setIsLoading(false);
+      console.log(data);
+    } catch (error) {
+      setError('An unexpected error occurred');
+      setIsLoading(false);
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
-        <input 
-          type="text" 
-          placeholder="username" 
+      {error && <div className="text-red-500 mb-4">{error}</div>}
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
           className="border p-3 rounded-lg"
           id="username"
-          />
-        <input 
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <input
           type="email"
-          placeholder="email" 
+          placeholder="Email"
           className="border p-3 rounded-lg"
           id="email"
-           />
-        <input 
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
           type="password"
-          placeholder="password" 
+          placeholder="Password"
           className="border p-3 rounded-lg"
           id="password"
-          />
-          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button
+          disabled={isLoading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {isLoading ? 'Loading...' : 'Sign Up'}
+        </button>
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to='/login'>
-          <span className='text-blue-700'>Login</span>
+        <Link to="/login">
+          <span className="text-blue-700">Login</span>
         </Link>
       </div>
     </div>
-  )
+  );
 }
