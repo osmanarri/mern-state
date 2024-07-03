c51abc45acd41b81ad703ce6016b982e13ee3f00
osmanarri <osmanarri1984@gmail.com>
2024-07-03 10:30:54 -0400

    Update the Header & make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './pages/Login'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 
 function App() {
@@ -17,7 +18,11 @@ function App() {
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/about' element={<About/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+        {/* Protect profile page */}
+        <Route element={<PrivateRoute/>}>
+          <Route path='/profile' element={<Profile/>}/>
+        </Route>
+        
       </Routes>
     </BrowserRouter>
     
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,43 @@
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
-  return (
-    <header className="bg-slate-200 shadow-md ">
-        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-            <Link to='/'>
-                <h1 className="font-bold text-sm sm:text-xl flex flex-wrip cursor-pointer">
-                <span className="text-slate-500">Osman</span>
-                <span className="text-slate-700">Estate</span>
-                </h1>
-            </Link>
-            <form className="bg-slate-100 p-3 rounded-lg flex items-center">
-                <input type="text" 
-                    placeholder="Search..." 
-                    className="bg-transparent focus:outline-none w-24 sm:w-64"/>
-                <FaSearch className="text-slate-600 cursor-pointer"/>
-            </form>
-            <ul className='flex gap-4'>
-                <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li></Link>
-                <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li></Link>
-                <Link to='/login'><li className='text-slate-700 hover:underline cursor-pointer'>Login</li></Link>               
-            </ul>
-        </div>    
-    </header>
-  )
+    const { currentUser } = useSelector(state => state.user);
+    
+    return (
+        <header className="bg-slate-200 shadow-md ">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                <Link to='/'>
+                    <h1 className="font-bold text-sm sm:text-xl flex flex-wrap cursor-pointer">
+                        <span className="text-slate-500">Osman</span>
+                        <span className="text-slate-700">Estate</span>
+                    </h1>
+                </Link>
+                <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                    <input 
+                        type="text" 
+                        placeholder="Search..." 
+                        className="bg-transparent focus:outline-none w-24 sm:w-64"
+                    />
+                    <FaSearch className="text-slate-600 cursor-pointer"/>
+                </form>
+                <ul className='flex gap-4'>
+                    <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li></Link>
+                    <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li></Link>
+                    <Link to='/profile'>
+                        {currentUser ? (
+                            <img 
+                                src={currentUser.avatar} 
+                                alt="profile-picture" 
+                                className="w-8 h-8 rounded-full"
+                            />
+                        ) : (
+                            <li className='text-slate-700 hover:underline cursor-pointer'>Login</li>
+                        )}
+                    </Link>
+                </ul>
+            </div>    
+        </header>
+    )
 }
