891dcb80a9153186be044973e1bb7a5bf00ed348
osmanarri <osmanarri1984@gmail.com>
2024-07-04 08:48:37 -0400

    complete profile page UI

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -6,6 +6,7 @@ export default function PrivateRoute() {
 
   return (
     <>
+    {/* Outlet is the childern of the parent component (PrivateRoute) */}
         {currentUser ? <Outlet/> : <Navigate to='/login'/>}
     </>
     
